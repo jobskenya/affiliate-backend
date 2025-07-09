daa0431a2aa9372a06c1aab8e440685e44c2998d
jobskenya <mavisjohnson4155@gmail.com>
2025-07-10 01:34:13 +0300

    Update activationRoutes.js

--- activationRoutes.js
+++ activationRoutes.js
@@ -3,7 +3,23 @@ const router = express.Router();
 const activationController = require('./activationController');
 const authMiddleware = require('./authMiddleware');
 
-router.post('/activate', authMiddleware, activationController.initiateSTKPush);
-router.post('/mpesa-callback', activationController.handleMPesaCallback);
+// Add status check middleware
+router.use(authMiddleware);
+
+// Initiate activation payment
+router.post('/activate', 
+  activationController.validateActivation,
+  activationController.initiateSTKPush
+);
+
+// M-Pesa callback handler
+router.post('/mpesa-callback', 
+  activationController.verifyCallback,
+  activationController.handleMPesaCallback,
+  activationController.activateUser
+);
+
+// Check activation status
+router.get('/status', activationController.checkActivationStatus);
 
 module.exports = router;
